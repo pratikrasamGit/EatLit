ec973369903980490887fec34842396f0440a5a1
pratikrasamGit <158097151+pratikrasamGit@users.noreply.github.com>
2024-05-13 20:04:54 +0530

    text  commit

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -43,11 +43,11 @@ export default function Login() {
       <div className='container'>
         <form className='w-50 m-auto mt-5 border bg-dark border-success rounded' onSubmit={handleSubmit}>
           <div className="m-3">
-            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+            <label htmlFor="exampleInputEmail1" className="form-label text-white">Email address</label>
             <input type="email" className="form-control" name='email' value={credentials.email} onChange={onChange} aria-describedby="emailHelp" />
           </div>
           <div className="m-3">
-            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+            <label htmlFor="exampleInputPassword1" className="form-label text-white">Password</label>
             <input type="password" className="form-control" value={credentials.password} onChange={onChange} name='password' />
           </div>
           <button type="submit" className="m-3 btn btn-success">Submit</button>
