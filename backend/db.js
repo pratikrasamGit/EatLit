e25ba903c5cd03a60e2666a325a4e62372331d2c
pratikrasamGit <158097151+pratikrasamGit@users.noreply.github.com>
2024-05-21 21:33:27 +0530

    mongo err  commit

--- backend/db.js
+++ backend/db.js
@@ -9,13 +9,10 @@ const mongoURI = 'mongodb+srv://foodapp:bpaKP9TS3gNBfo88@cluster0.ldmy2hh.mongod
 //     useCreateIndex: true
 
 //   }
-  var options = { server:
-    { socketOptions: 
-         { 
-             socketTimeoutMS: 15000, 
-             connectTimeoutMS: 15000 
-         }
-     }
+  var options = { 
+    connectTimeoutMS: 5000,
+    socketTimeoutMS: 5000,
+    useUnifiedTopology: true
    };
 // mongodb://<username>:<password>@merncluster-shard-00-00.d1d4z.mongodb.net:27017,merncluster-shard-00-01.d1d4z.mongodb.net:27017,merncluster-shard-00-02.d1d4z.mongodb.net:27017/?ssl=true&replicaSet=atlas-eusy5p-shard-0&authSource=admin&retryWrites=true&w=majority
 module.exports = function (callback) {
