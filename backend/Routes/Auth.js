bcd66b6a2296e76e072ba1ad8228d342eb90b661
pratikrasamGit <158097151+pratikrasamGit@users.noreply.github.com>
2024-09-04 05:10:09 +0530

    loading bug  commit

--- backend/Routes/Auth.js
+++ backend/Routes/Auth.js
@@ -154,7 +154,7 @@ router.post('/foodData', async (req, res) => {
 
     } catch (error) {
         console.error(error.message)
-        res.send("Server Error",error.message)
+        res.send("Server Error")
 
     }
 })
